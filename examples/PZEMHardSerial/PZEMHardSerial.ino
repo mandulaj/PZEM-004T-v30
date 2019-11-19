fd04223b34487a813c0283d67c0cfcd63a1cdb82
Jakub Mandula <jakub@mandula.cz>
2019-11-19 21:30:54 +0100

    Fix the Hardware Serial example like in #14

--- examples/PZEMHardSerial/PZEMHardSerial.ino
+++ examples/PZEMHardSerial/PZEMHardSerial.ino
@@ -8,42 +8,42 @@ void setup() {
 
 void loop() {
     float voltage = pzem.voltage();
-    if(voltage != NAN){
+    if(!isnan(voltage)){
         Serial.print("Voltage: "); Serial.print(voltage); Serial.println("V");
     } else {
         Serial.println("Error reading voltage");
     }
 
     float current = pzem.current();
-    if(current != NAN){
+    if(!isnan(current)){
         Serial.print("Current: "); Serial.print(current); Serial.println("A");
     } else {
         Serial.println("Error reading current");
     }
 
     float power = pzem.power();
-    if(current != NAN){
+    if(!isnan(power)){
         Serial.print("Power: "); Serial.print(power); Serial.println("W");
     } else {
         Serial.println("Error reading power");
     }
 
     float energy = pzem.energy();
-    if(current != NAN){
+    if(!isnan(energy)){
         Serial.print("Energy: "); Serial.print(energy,3); Serial.println("kWh");
     } else {
         Serial.println("Error reading energy");
     }
 
     float frequency = pzem.frequency();
-    if(current != NAN){
+    if(!isnan(frequency)){
         Serial.print("Frequency: "); Serial.print(frequency, 1); Serial.println("Hz");
     } else {
         Serial.println("Error reading frequency");
     }
 
     float pf = pzem.pf();
-    if(current != NAN){
+    if(!isnan(pf)){
         Serial.print("PF: "); Serial.println(pf);
     } else {
         Serial.println("Error reading power factor");
